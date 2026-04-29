35ad396c94180d147b12c228d4e334df402e4c3f
shiv78050 <shivamsingh74r@gmail.com>
2026-04-29 09:27:14 +0530

    adding a file

---
+++ Python/1_string.py
@@ -0,0 +1,9 @@
+-- write a python program to reverse words of sentence in same order
+def reverse_words(sentence):
+    words = sentence.split()
+    reversed_words = [word[::-1] for word in words]
+    return ' '.join(reversed_words)
+# Example usage
+input_sentence = "Hello World"
+reversed_sentence = reverse_words(input_sentence)
+print(reversed_sentence)  # Output: "olleH dlroW"
